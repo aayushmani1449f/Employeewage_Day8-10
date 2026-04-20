1b1d54461f769c9327211061c5f879e0b0f0f7e9
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-20 16:38:46 +0530

    UC7: Refactor the Code to write a Class Method

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -1,4 +1,5 @@
 package com.employeewage;
+
 public class EmployeeWageComputation {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
@@ -6,8 +7,7 @@ public class EmployeeWageComputation {
     public static final int NUM_OF_WORKING_DAYS = 20;
     public static final int MAX_HRS_IN_MONTH = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    public static void computeEmpWage() {
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
@@ -30,5 +30,10 @@ public class EmployeeWageComputation {
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         System.out.println("Total Emp Wage: " + totalEmpWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        computeEmpWage();
     }
 }
