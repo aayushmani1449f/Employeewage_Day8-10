21e4f389fe5d8fccbdc28f67da8c7fcb4a8df8c6
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-20 16:38:27 +0530

    UC3: Add Part time Employee

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -2,12 +2,15 @@ package com.employeewage;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        if (empCheck == IS_PART_TIME)
+            empHrs = 8;
+        else if (empCheck == IS_FULL_TIME)
             empHrs = 8;
         else
             empHrs = 0;
