dfa1379ae3de95bcbe2a88cb713627324e968aa9
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-22 11:29:20 +0530

    UC12: Refactor to use ArrayList instead of Array

--- src/com/employeewage/EmpWageBuilder.java
+++ src/com/employeewage/EmpWageBuilder.java
@@ -1,27 +1,28 @@
 package com.employeewage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmpWageBuilder implements IComputeEmpWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
-    private int numOfCompany = 0;
-    private CompanyEmpWage[] companyEmpWageArray;
+    private List<CompanyEmpWage> companyEmpWageList;
 
     public EmpWageBuilder() {
-        companyEmpWageArray = new CompanyEmpWage[5];
+        companyEmpWageList = new ArrayList<>();
     }
 
     @Override
     public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-        companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
-        numOfCompany++;
+        companyEmpWageList.add(new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth));
     }
 
     @Override
     public void computeEmpWage() {
-        for (int i = 0; i < numOfCompany; i++) {
-            companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-            System.out.println(companyEmpWageArray[i]);
+        for (CompanyEmpWage companyEmpWage : companyEmpWageList) {
+            companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+            System.out.println(companyEmpWage);
         }
     }
 
