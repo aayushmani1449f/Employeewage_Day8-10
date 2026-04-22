447ef7568c76621e6604edf04ee356fede2bb90a
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-22 11:28:36 +0530

    UC11: Implement IComputeEmpWage interface

--- src/com/employeewage/EmpWageBuilder.java
+++ src/com/employeewage/EmpWageBuilder.java
@@ -1,6 +1,6 @@
 package com.employeewage;
 
-public class EmpWageBuilder {
+public class EmpWageBuilder implements IComputeEmpWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
@@ -11,12 +11,14 @@ public class EmpWageBuilder {
         companyEmpWageArray = new CompanyEmpWage[5];
     }
 
-    private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+    @Override
+    public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
         companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         numOfCompany++;
     }
 
-    private void computeEmpWage() {
+    @Override
+    public void computeEmpWage() {
         for (int i = 0; i < numOfCompany; i++) {
             companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
             System.out.println(companyEmpWageArray[i]);
---
+++ src/com/employeewage/IComputeEmpWage.java
@@ -0,0 +1,6 @@
+package com.employeewage;
+
+public interface IComputeEmpWage {
+    public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    public void computeEmpWage();
+}
